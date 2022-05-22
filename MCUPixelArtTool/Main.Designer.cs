b3099d33888fca5328d3a58a4c6d9a5c073b9d2b
u08614s <94842027+u08614s@users.noreply.github.com>
2022-05-22 21:52:10 +0900

    Add files via upload

---
+++ MCUPixelArtTool/Main.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace PixelArtTool {
+    partial class Main {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.lblResult = new System.Windows.Forms.Label();
+            this.tbxPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.ofdOpen = new System.Windows.Forms.OpenFileDialog();
+            this.lblSource = new System.Windows.Forms.Label();
+            this.btnPaste = new System.Windows.Forms.Button();
+            this.IPBox = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.dgvColors = new System.Windows.Forms.DataGridView();
+            this.ColorColumn = new System.Windows.Forms.DataGridViewImageColumn();
+            this.RGBColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BlockColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DataColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ofdLoad = new System.Windows.Forms.OpenFileDialog();
+            this.cdChooseColor = new System.Windows.Forms.ColorDialog();
+            this.interpolatedPictureBox1 = new PixelArtTool.Controls.InterpolatedPictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvColors)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.interpolatedPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblResult
+            // 
+            this.lblResult.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(471, 51);
+            this.lblResult.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(76, 18);
+            this.lblResult.TabIndex = 1;
+            this.lblResult.Text = "プレビュー";
+            // 
+            // tbxPath
+            // 
+            this.tbxPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxPath.Location = new System.Drawing.Point(20, 17);
+            this.tbxPath.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.tbxPath.Name = "tbxPath";
+            this.tbxPath.Size = new System.Drawing.Size(664, 25);
+            this.tbxPath.TabIndex = 2;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(697, 15);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(50, 32);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.LoadImage);
+            // 
+            // ofdOpen
+            // 
+            this.ofdOpen.Filter = "PNG files|*.png";
+            this.ofdOpen.Title = "Select an image...";
+            // 
+            // lblSource
+            // 
+            this.lblSource.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(236, 51);
+            this.lblSource.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(62, 18);
+            this.lblSource.TabIndex = 1;
+            this.lblSource.Text = "元画像";
+            // 
+            // btnPaste
+            // 
+            this.btnPaste.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPaste.Enabled = false;
+            this.btnPaste.Location = new System.Drawing.Point(19, 267);
+            this.btnPaste.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.Size = new System.Drawing.Size(728, 32);
+            this.btnPaste.TabIndex = 4;
+            this.btnPaste.Text = "送信";
+            this.btnPaste.UseVisualStyleBackColor = true;
+            this.btnPaste.Click += new System.EventHandler(this.ConvertImage);
+            // 
+            // IPBox
+            // 
+            this.IPBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.IPBox.Location = new System.Drawing.Point(20, 312);
+            this.IPBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.IPBox.Name = "IPBox";
+            this.IPBox.Size = new System.Drawing.Size(562, 25);
+            this.IPBox.TabIndex = 2;
+            this.IPBox.Text = "192.168.";
+            this.IPBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConnect.Location = new System.Drawing.Point(592, 312);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(152, 30);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "接続";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.Connect);
+            // 
+            // dgvColors
+            // 
+            this.dgvColors.AllowUserToAddRows = false;
+            this.dgvColors.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvColors.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvColors.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColorColumn,
+            this.RGBColumn,
+            this.BlockColumn,
+            this.DataColumn});
+            this.dgvColors.Location = new System.Drawing.Point(697, 111);
+            this.dgvColors.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.dgvColors.Name = "dgvColors";
+            this.dgvColors.RowHeadersWidth = 62;
+            this.dgvColors.Size = new System.Drawing.Size(10, 85);
+            this.dgvColors.TabIndex = 11;
+            this.dgvColors.Visible = false;
+            // 
+            // ColorColumn
+            // 
+            this.ColorColumn.FillWeight = 30F;
+            this.ColorColumn.HeaderText = "";
+            this.ColorColumn.MinimumWidth = 8;
+            this.ColorColumn.Name = "ColorColumn";
+            this.ColorColumn.ReadOnly = true;
+            this.ColorColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.ColorColumn.Width = 30;
+            // 
+            // RGBColumn
+            // 
+            this.RGBColumn.HeaderText = "RGB";
+            this.RGBColumn.MinimumWidth = 8;
+            this.RGBColumn.Name = "RGBColumn";
+            this.RGBColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.RGBColumn.Width = 150;
+            // 
+            // BlockColumn
+            // 
+            this.BlockColumn.HeaderText = "Block ID";
+            this.BlockColumn.MinimumWidth = 8;
+            this.BlockColumn.Name = "BlockColumn";
+            this.BlockColumn.ReadOnly = true;
+            this.BlockColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.BlockColumn.Width = 150;
+            // 
+            // DataColumn
+            // 
+            this.DataColumn.HeaderText = "Block Data ID";
+            this.DataColumn.MinimumWidth = 8;
+            this.DataColumn.Name = "DataColumn";
+            this.DataColumn.ReadOnly = true;
+            this.DataColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.DataColumn.Width = 150;
+            // 
+            // ofdLoad
+            // 
+            this.ofdLoad.Filter = "Color Configurations|*.pxl";
+            this.ofdLoad.Title = "Where is your color configuration located?";
+            // 
+            // interpolatedPictureBox1
+            // 
+            this.interpolatedPictureBox1.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.Default;
+            this.interpolatedPictureBox1.Location = new System.Drawing.Point(239, 100);
+            this.interpolatedPictureBox1.Name = "interpolatedPictureBox1";
+            this.interpolatedPictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.interpolatedPictureBox1.TabIndex = 0;
+            this.interpolatedPictureBox1.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 350);
+            this.Controls.Add(this.interpolatedPictureBox1);
+            this.Controls.Add(this.dgvColors);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnPaste);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.IPBox);
+            this.Controls.Add(this.tbxPath);
+            this.Controls.Add(this.lblSource);
+            this.Controls.Add(this.lblResult);
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Main";
+            this.Text = "シンプルドット絵ツール";
+            this.Load += new System.EventHandler(this.FormLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvColors)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.interpolatedPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox tbxPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.OpenFileDialog ofdOpen;
+        private System.Windows.Forms.Label lblSource;
+        private System.Windows.Forms.Button btnPaste;
+        private System.Windows.Forms.TextBox IPBox;
+        private System.Windows.Forms.Button btnConnect;
+        private PixelArtTool.Controls.InterpolatedPictureBox imgSource;
+        private PixelArtTool.Controls.InterpolatedPictureBox imgResult;
+        private System.Windows.Forms.DataGridView dgvColors;
+        private System.Windows.Forms.OpenFileDialog ofdLoad;
+        private System.Windows.Forms.DataGridViewImageColumn ColorColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RGBColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BlockColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DataColumn;
+        private System.Windows.Forms.ColorDialog cdChooseColor;
+        private Controls.InterpolatedPictureBox interpolatedPictureBox1;
+    }
+}
+
---
+++ MCUPixelArtTool/Main.cs
@@ -0,0 +1,233 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PixelArtTool
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        public Gecko GeckoU;
+        public static Dictionary<Color, int[]> customColourData = new Dictionary<Color, int[]>();
+
+        public static Image ResizeImage(Image srcImg, Size newSize)
+        {
+            return new Bitmap(srcImg, newSize);
+        }
+
+        private void LoadImage(object sender, EventArgs e)
+        {
+            if (dgvColors.Rows.Count <= 1)
+            {
+                MessageBox.Show("Please provide at least two colors.", "Error", MessageBoxButtons.OK);
+                return;
+            }
+            var dialogRes = ofdOpen.ShowDialog();
+            if (dialogRes == DialogResult.OK)
+            {
+                customColourData.Clear();
+                tbxPath.Text = ofdOpen.FileName;
+                var image = Image.FromFile(ofdOpen.FileName);
+                imgSource.Image = ResizeImage(image, new Size((int)128, (int)128));
+                var colourList = new List<Color>();
+                List<Color> sourceColours = new List<Color>();
+                foreach (DataGridViewRow row in dgvColors.Rows)
+                {
+                    var grab = row.Cells[1].Value.ToString().Split(',');
+                    sourceColours.Add(Color.FromArgb(int.Parse(grab[0]), int.Parse(grab[1]), int.Parse(grab[2])));
+                    customColourData.Add(Color.FromArgb(int.Parse(grab[0]), int.Parse(grab[1]), int.Parse(grab[2])), new[] { int.Parse(row.Cells[2].Value.ToString()), int.Parse(row.Cells[3].Value.ToString()) });
+                }
+                var image2 = new Bitmap(imgSource.Image);
+                for (int y = 0; y < (int)128; y++)
+                {
+                    for (int x = 0; x < (int)128; x++)
+                    {
+                        colourList.Add(GetClosestColor(sourceColours.ToArray(), image2.GetPixel(x, y)));
+                    }
+                }
+                var result = GenerateImage((int)128, (int)128, colourList.ToArray());
+                imgResult.Image = result;
+                dgvColors.ReadOnly = true;
+            }
+        }
+
+        private void ConvertImage(object sender, EventArgs e)
+        {
+            if (imgResult.Image == null)
+            {
+                MessageBox.Show("画像が選ばれていません", "シンプルドット絵ツール", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+
+                var img = new Bitmap(imgResult.Image);
+
+                var size = (int)128 * (int)128;
+                var length = 0x3D000000 + ((size * 2) * 32);
+
+                var x = 0;
+                var y = 0;
+                var z = 0;
+
+                List<uint> blockList = new List<uint>();
+
+                for (int nx = (int)128 - 1; nx > -1; nx--)
+                {
+                    y = 0;
+                    for (int ny = 0; ny < 2; ny++)
+                    {
+                        z = 0;
+                        for (int nz = (int)128 - 1; nz > -1; nz--)
+                        {
+                            if (y > 0)
+                            {
+                                blockList.AddRange(new List<uint>() { ReverseBytes((uint)x), ((uint)y), ReverseBytes((uint)z), 0, 0, 0, 0, 0 });
+                            }
+                            else
+                            {
+                                if (customColourData.TryGetValue(img.GetPixel(nx, nz), out int[] data))
+                                {
+                                    blockList.AddRange(new List<uint>() { ReverseBytes((uint)x), ReverseBytes((uint)y), ReverseBytes((uint)z), ReverseBytes((uint)data[0]), ReverseBytes((uint)data[1]), 0, 0, 0 });
+                                }
+                                else
+                                {
+                                    if (img.GetPixel(nx, nz) == Color.FromArgb(0, 0, 0, 0))
+                                    {
+                                        blockList.AddRange(new List<uint>() { ReverseBytes((uint)x), ReverseBytes((uint)y), ReverseBytes((uint)z), 0, 0, 0, 0, 0 });
+                                    }
+                                    else
+                                    {
+                                        blockList.AddRange(new List<uint>() { ReverseBytes((uint)x), ReverseBytes((uint)y), ReverseBytes((uint)z), ReverseBytes(251u), ReverseBytes(15), 0, 0, 0 });
+                                    }
+                                }
+                            }
+                            z++;
+                        }
+                        y++;
+                    }
+                    x++;
+                }
+
+                var newBlockList = blockList.ToArray();
+
+                byte[] blockData = new byte[newBlockList.Length * 4];
+                Buffer.BlockCopy(newBlockList, 0, blockData, 0, newBlockList.Length * 4);
+
+                Console.WriteLine(BitConverter.ToString(blockData));
+
+                GeckoU.PokeInt(0x3A100030, (int)GeckoU.PeekUInt(GeckoU.PeekUInt(GeckoU.PeekUInt(0x109CD8E4) + 0x34) + 0x54));
+                GeckoU.PokeInt(0x3A100034, (int)GeckoU.PeekUInt(GeckoU.PeekUInt(GeckoU.PeekUInt(0x109CD8E4) + 0x34) + 0x58));
+                GeckoU.PokeInt(0x3A100038, (int)GeckoU.PeekUInt(GeckoU.PeekUInt(GeckoU.PeekUInt(0x109CD8E4) + 0x34) + 0x5C));
+                GeckoU.WriteBytes(0x3D000000, blockData);
+                GeckoU.PokeUInt(0x12000000, (uint)length);
+                GeckoU.WriteBytes(0x04000000, Data.set());
+                GeckoU.CallFunction(0x04000000, new uint[1]);
+                MessageBox.Show("送信しました", "シンプルドット絵ツール", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        public static UInt32 ReverseBytes(UInt32 value)
+        {
+            return (value & 0x000000FFU) << 24 | (value & 0x0000FF00U) << 8 |
+                (value & 0x00FF0000U) >> 8 | (value & 0xFF000000U) >> 24;
+        }
+
+        public Bitmap GenerateImage(int width, int height, Color[] colours)
+        {
+            Bitmap image = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            var uindice = 0;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    image.SetPixel(x, y, colours[uindice]);
+                    uindice++;
+                }
+            }
+
+            return image;
+        }
+
+        private Color GetClosestColor(Color[] colors, Color c)
+        {
+            Color closestColour = new Color();
+            double smallestDiff = double.MaxValue;
+            double colourDiff;
+
+            foreach (Color currentColour in colors)
+            {
+                colourDiff = Math.Abs(c.R - currentColour.R) + Math.Abs(c.G - currentColour.G) + Math.Abs(c.B - currentColour.B);
+                if (colourDiff < smallestDiff)
+                {
+                    smallestDiff = colourDiff;
+                    closestColour = currentColour;
+                }
+            }
+
+            if (c.A == 0)
+            {
+                closestColour = Color.FromArgb(0, 0, 0, 0);
+            }
+
+            return closestColour;
+        }
+
+        private void Connect(object hass, EventArgs e)
+        {
+            GeckoU = new Gecko(IPBox.Text);
+            try
+            {
+                GeckoU.tcpConn.Connect();
+                btnConnect.Text = "接続済み";
+                btnConnect.Enabled = false;
+                string text = "9421FFF093E1000C3FE03B00813F00002C090001418200403D20025A7C0802A661291AD0900100147D2903A64E8004213D2002296129CFFC386000017D2903A64E8004218001001439200001913F00007C0803A683E1000C382100104E800020";
+                GeckoU.WriteBytes(0x03900000, GeckoU.string2bytes(text));
+                GeckoU.CallFunction(0x03900000, new uint[1]);
+                btnPaste.Enabled = true;
+            }
+            catch (Exception value)
+            {
+                Console.WriteLine(value);
+            }
+        }
+
+        private void FormLoad(object sender, EventArgs e)
+        {
+            //This is wool colors.
+            AddColorEntry(ColorTranslator.FromHtml("#FFE4E4E4"), "35", "0");
+            AddColorEntry(ColorTranslator.FromHtml("#FFEA7E35"), "35", "1");
+            AddColorEntry(ColorTranslator.FromHtml("#FFBE49C9"), "35", "2");
+            AddColorEntry(ColorTranslator.FromHtml("#FF6387D2"), "35", "3");
+            AddColorEntry(ColorTranslator.FromHtml("#FFC2B51C"), "35", "4");
+            AddColorEntry(ColorTranslator.FromHtml("#FF39BA2E"), "35", "5");
+            AddColorEntry(ColorTranslator.FromHtml("#FFD98199"), "35", "6");
+            AddColorEntry(ColorTranslator.FromHtml("#FF414141"), "35", "7");
+            AddColorEntry(ColorTranslator.FromHtml("#FFA0A7A7"), "35", "8");
+            AddColorEntry(ColorTranslator.FromHtml("#FF267191"), "35", "9");
+            AddColorEntry(ColorTranslator.FromHtml("#FF7E34BF"), "35", "10");
+            AddColorEntry(ColorTranslator.FromHtml("#FF253193"), "35", "11");
+            AddColorEntry(ColorTranslator.FromHtml("#FF56331C"), "35", "12");
+            AddColorEntry(ColorTranslator.FromHtml("#FF364B18"), "35", "13");
+            AddColorEntry(ColorTranslator.FromHtml("#FF9E2B27"), "35", "14");
+            AddColorEntry(ColorTranslator.FromHtml("#FF181414"), "35", "15");
+        }
+
+        private void AddColorEntry(Color color, string blockID, string blockData)
+        {
+            Bitmap bmp = new Bitmap(21, 21);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(color);
+                dgvColors.Rows.Add(bmp, $"{color.R},{color.G},{color.B}", blockID, blockData);
+            }
+        }
+    }
+}
